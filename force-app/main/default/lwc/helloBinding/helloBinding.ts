d24b77a915c7dbc3636c06d779b6e871dd4ca44f
David Turissini <dturissini@salesforce.com>
2019-05-21 22:55:31 -0700

    using external typescript module

--- force-app/main/default/lwc/helloBinding/helloBinding.ts
+++ force-app/main/default/lwc/helloBinding/helloBinding.ts
@@ -1,7 +1,7 @@
 import { LightningElement, track } from 'lwc';
-
+import { test } from './library';
 export default class HelloBinding extends LightningElement {
-    @track greeting: string = 'World';
+    @track greeting: string = 'World ' + test();
 
     handleChange(event: Event) {
         this.greeting = (event.target as HTMLInputElement).value;
